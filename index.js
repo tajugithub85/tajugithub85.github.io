0bfc8cf65edc81560fc60f3a8369fd46d391c6f1
tajugithub85clear <62436203+tajugithub85@users.noreply.github.com>
2020-07-11 12:02:11 +0100

    js weekly challenges

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+  var sumof3 = prompt("sum of multiple of 3",  " ");
+  var sumof5 = prompt("sum of multiple of 5", " ");
+  var sum = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % sumof3 === 0 || x % sumof5 === 0)
+    {
+       sum += x;
+    }
+}
+alert("the sum of multiple of 3 and 5 is  " + sum);
---
+++ index2.js
@@ -0,0 +1,21 @@
+var arr1 = window.prompt("Print the Frquency No."); 
+var arr2 = arr1.toString();
+var arr3 = [arr2[0]];
+var mf = 1;
+var m = 0;
+var item;
+for (var i=0; i<arr2.length; i++)
+{
+        for (var j=i; j<arr2.length; j++)
+        {
+                if (arr2[i] == arr2[j])
+                 m++;
+                if (mf<m)
+                { 
+                  mf=m; 
+                  item = arr2[i];
+                }
+        }
+        m=0;
+}
+alert(item+" ( " +mf +" times ) ") ;
